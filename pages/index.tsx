d1f4aa4e43bb0c7589790b9294a5a90aa5b6bfb7
harutete <ai.takeuchi.10@gmail.com>
2021-06-06 23:11:44 +0900

    修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,15 @@
 import * as THREE from 'three';
-import { useRef, Suspense } from 'react';
+import { Suspense } from 'react';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-import {
-  ReactThreeFiber,
-  Canvas,
-  useLoader,
-  useThree,
-  extend
-} from '@react-three/fiber';
-import { OrbitControls } from 'three-stdlib';
-
-extend({ OrbitControls });
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      orbitControls: ReactThreeFiber.Node<OrbitControls, typeof OrbitControls>;
-    }
-  }
-}
+import { Canvas, useLoader } from '@react-three/fiber';
 
 const FbxFile = () => {
-  const controls = useRef<OrbitControls>();
-  const { camera, gl } = useThree();
   const fbx = useLoader(FBXLoader, '/fbx/sample.fbx');
 
   console.log({ fbx });
   return (
     <primitive
+      onClick={clickHandler}
       object={fbx}
       dispose={null}
       scale={[0.8, 0.8, 0.8]}
@@ -53,15 +35,9 @@ const SampleTexture = () => {
 };
 
 const Home = () => {
-  const onMouseMove = () => {
-    console.log('move');
-  };
   return (
     <div style={{ backgroundColor: '#000000' }}>
-      <Canvas
-        onMouseMove={onMouseMove}
-        style={{ width: '100vw', height: '100vh' }}
-      >
+      <Canvas style={{ width: '100vw', height: '100vh' }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Suspense fallback={null}>
